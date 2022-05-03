9b05199e06156829f5ea564f6dfe27627c0eed7d
OVECJOE <ovecjoe123@gmail.com>
2022-05-03 15:27:25 +0100

    Task 3 done

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,11 +1,10 @@
 import "../css/main.css";
-
 const $ = require("jquery");
 const _ = require("lodash");
 
 let count = 0;
 
-$('body').append($("<div id='logo'></div>"));
+$('body').append($("<div id='logo'> </div>"));
 $('body').append($("<p>Holberton Dashboard</p>"));
 $('body').append($("<p>Dashboard data for the students</p>"));
 $('body').append($("<button type=button>Click here to get started</button>"));
