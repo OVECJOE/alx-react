920566bcbf21bb0d0a9d99dff11da1244553d5af
OVECJOE <ovecjoe123@gmail.com>
2022-05-03 12:30:22 +0100

    Still fixing Task 0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
-const $ = require("jquery");
+import $ from "jquery";
 
-$('body').add($('<p>Holberton Dashboard</p>'));
-$('body').add($('<p>Dashboard data for the students</p>'));
-$('body').add($('<p>Copyright - Holberton School</p>'));
+$('body').append($("<p>Holberton Dashboard</p>"));
+$('body').append($("<p>Dashboard data for the students</p>"));
+$('body').append($("<p>Copyright - Holberton School</p>"));
