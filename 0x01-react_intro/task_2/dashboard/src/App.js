07c3b93c212a2bf1a6ea90cbff0e7dd4a13f9ddf
OVECJOE <ovecjoe123@gmail.com>
2022-05-27 22:43:50 +0100

    Done task 2

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <label htmlFor='email'>Enter your email: </label>
+        <input type='email' id='email' name='email' />
+        <label htmlFor='password'>Enter your password: </label>
+        <input type='password' id='password' name='password' />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
