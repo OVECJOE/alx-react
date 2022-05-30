c2801d558f26eaf034f56608e2030e7ceae6ca94
OVECJOE <ovecjoe123@gmail.com>
2022-05-30 09:59:30 +0100

    Task 10 done

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.jpg';
 import './App.css';
-import { getFullYear, getFooterCopy } from '../utils';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
